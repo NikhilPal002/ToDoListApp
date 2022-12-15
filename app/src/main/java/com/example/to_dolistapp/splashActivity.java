ef9d32e29053433ebbd95aab8325514181166411
Nikhil <nikhilpal2017@gmail.com>
2022-12-15 23:35:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/to_dolistapp/splashActivity.java
@@ -0,0 +1,29 @@
+package com.example.to_dolistapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 4000);
+    }
+}
